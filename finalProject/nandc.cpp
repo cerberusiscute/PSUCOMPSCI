b6d9154884dba18db451b64266514e6834db01c2
DESKTOP-3TDELON\Mark <mbbailey96@gmail.com>
2018-02-09 17:43:38 -0500

    again

--- finalProject/nandc.cpp
+++ finalProject/nandc.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
 * add shortcurcuit to checks for win and for checks for each winning combo to max efficiency
 */
@@ -8,7 +7,25 @@
 
 using namespace std;
 
-char board[9] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+// FUNCTION DECLARATIONS
+void validate(int, char); // function to decide if valid move (not already filled)
+void displayBoard(char*); // print board
+bool winCon(char*, char); // check for wins
+void fillSquare(char); // insert 'x' or 'o' into board
+
+char board[9] = {'0', '1', '2', '3', '4', '5', '6', '7', '8'}; // array for tracking board
+
+void validate(int move, char player) // move should always be filled with board[x]
+{
+	if (char(move) == 'x' || char(move) == 'o') // simply check if equal to already placed character
+	{
+		cout << endl << "Please select a different spot to place your " << player << endl;
+		system("pause");
+		system("cls");
+		displayBoard(board);
+		fillSquare(player);
+	}
+}
 
 void displayBoard(char b[9])
 {
@@ -21,7 +38,7 @@ void displayBoard(char b[9])
 			cout << "\t\t" << "-------------" << endl;
 		}
 	}
-}
+} // end displayBoard
 
 bool winCon(char board[9], char player)
 {
@@ -45,8 +62,14 @@ bool winCon(char board[9], char player)
 		}
 	}
 
+	// check diags
+	if (board[0] == board[4] && board [8] == board[4] || board[2] == board[4] && board[6] == board[4]) // checks to see if board[4] (center) is equal to either set of diag corners
+	{
+		win == true;
+	}
+
 	return win;
-}
+} // end winCon
 
 void fillSquare(char player)
 {
@@ -55,8 +78,9 @@ void fillSquare(char player)
 	{
 		if (player == 'x')
 		{
-			cout << "X, make your move!\t";
+			cout << endl << "X, make your move!\t";
 			cin >> toFill;
+			validate(board[toFill], player);
 			board[toFill] = player;
 			system("cls");
 			displayBoard(board);
@@ -70,7 +94,9 @@ void fillSquare(char player)
 		}
 		else
 		{
+			cout << endl << "O, make your move!\t";
 			cin >> toFill;
+			validate(board[toFill], player);
 			board[toFill] = player;
 			system("cls");
 			displayBoard(board);
@@ -83,7 +109,7 @@ void fillSquare(char player)
 			player = 'x';
 		}
 	}
-}
+} // end fillSquare
 
 int main()
 {
@@ -93,101 +119,4 @@ int main()
 
 	system("pause");
 	return 0;
-}
-=======
-/*
-* add shortcurcuit to checks for win and for checks for each winning combo to max efficiency
-*/
-
-#include <iostream>
-#include <cstdlib>
-
-using namespace std;
-
-char board[9] = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
-
-void displayBoard(char b[9])
-{
-	cout << endl << endl << endl;
-	for (int i = 0; i < 8; i += 3)
-	{
-		cout << "\t\t" << "| " << b[int(i)] << " | " << b[int(i) + 1] << " | " << b[int(i) + 2] << " |" << endl;
-		if (!(i >= 6))
-		{
-			cout << "\t\t" << "-------------" << endl;
-		}
-	}
-}
-
-bool winCon(char board[9], char player)
-{
-	bool win = false;	
-
-	for (int i = 0; i < 9; i += 3)
-	{
-		if (board[i] == player && board[i+1] == player && board[i+2] == player)
-		{
-			win = true;
-			break;
-		}
-	}
-	
-	for (int c = 0; c < 3; c++)
-	{
-		if (board[c] == player && board[c+3] == player && board[c+6] == player)
-		{
-			win = true;
-			break;
-		}
-	}
-
-	return win;
-}
-
-void fillSquare(char player)
-{
-	int toFill;
-	for (int i = 0; i < 9; i++)
-	{
-		if (player == 'x')
-		{
-			cout << "X, make your move!\t";
-			cin >> toFill;
-			board[toFill] = player;
-			system("cls");
-			displayBoard(board);
-			bool win = winCon(board, player);
-			if (win == true)
-			{
-				cout << "X's win!!" << endl << endl;
-				break;
-			}
-			player = 'o';
-		}
-		else
-		{
-			cin >> toFill;
-			board[toFill] = player;
-			system("cls");
-			displayBoard(board);
-			bool win = winCon(board, player);
-			if (win == true)
-			{
-				cout << "O's win!!" << endl << endl;
-				break;
-			}
-			player = 'x';
-		}
-	}
-}
-
-int main()
-{
-	displayBoard(board);
-
-	fillSquare('x');
-
-	system("pause");
-	return 0;
-}
->>>>>>> 631cfa8895bf37745ea2ab608712380b4ae9f9a1
+} // end main
